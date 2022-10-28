72b5547523457b8469b39caa266e3b307431c15e
hmuppara@asu.edu <hmuppara@asu.edu>
2022-10-27 20:13:15 -0700

    Negative Test Cases for goodString()

--- src/UrinalsTest.java
+++ src/UrinalsTest.java
@@ -22,10 +22,23 @@ public class UrinalsTest {
         assertTrue(urinals.goodString("011"));
     }
 
+    @Test
+    void badString() {
+        Urinals urinals = new Urinals();
+        assertFalse(urinals.goodString("100201"));
+        assertFalse(urinals.goodString("00000000000000000000000000000000"));
+        assertFalse(urinals.goodString(""));
+        assertFalse(urinals.goodString("11111111111111111111111111111111"));
+        assertFalse(urinals.goodString("92393921419204"));
+        assertFalse(urinals.goodString("     "));
+        assertFalse(urinals.goodString("12417sd"));
+    }
+
     @Test()
     void main(){
         checkIfFileExists();
         goodString();
+        badString();
 
     }
 }
