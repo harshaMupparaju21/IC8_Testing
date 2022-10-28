b5af04d24da466e920af58229972888382253b9f
hmuppara@asu.edu <hmuppara@asu.edu>
2022-10-27 21:23:07 -0700

    TestCases for getting Free Urinal Count

--- src/Urinals.java
+++ src/Urinals.java
@@ -1,25 +1,68 @@
-import java.io.File;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
+import java.util.Scanner;
 
 /**
  * @author Harsha Vardhan Mupparaju
  */
 public class Urinals {
 
+    static List<String> urinalList = new ArrayList<>();
+
    public boolean checkIfFileExists(){
-       String resource = "Urinals.dat";
-       File file = new File(getClass().getClassLoader().getResource(resource).getFile());
-       return file.getAbsolutePath().endsWith("/Urinals.dat");
+       try {
+           String resource = "Urinals.dat";
+           File file = new File(getClass().getClassLoader().getResource(resource).getFile());
+           return file.getAbsolutePath().endsWith("/Urinals.dat");
+       } catch (NullPointerException e){
+           return false;
+       }
    }
 
    public boolean goodString(String input){
+       if(input.contains("11")){
+           return false;
+       }
        Pattern p = Pattern.compile("^[0-1]{1,20}$");
        Matcher m = p.matcher(input);
        return m.matches();
    }
 
-    public static void main(String[] args) {
+   public int countUrinals(String urinal) {
+       int count = 0;
+       if (goodString(urinal) == false) {
+           return -1;
+       } else {
+           int x, y = 0;
 
+           StringBuffer urinalBuffer = new StringBuffer(urinal);
+
+           for (int i = 0; i < urinalBuffer.length(); i++) {
+               if (urinalBuffer.charAt(i) == '0') {
+                   if (i == 0 || urinalBuffer.charAt(i - 1) == '0') {
+                       x = 1;
+                   } else {
+                       x = 0;
+                   }
+                   if (i == urinalBuffer.length() - 1 || urinalBuffer.charAt(i + 1) == '0') {
+                       y = 1;
+                   } else {
+                       y = 0;
+                   }
+                   if (x == 1 && y == 1) {
+                       count++;
+                       urinalBuffer.setCharAt(i, '1');
+                   }
+               }
+           }
+           return count;
+       }
+   }
+
+
+    public static void main(String[] args) {
     }
 }
--- src/UrinalsTest.java
+++ src/UrinalsTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
  * @author Harsha Vardhan Mupparaju
  */
 public class UrinalsTest {
-    @Test
+    @Test()
     void checkIfFileExists(){
         Urinals urinals = new Urinals();
         assertTrue(urinals.checkIfFileExists());
@@ -16,15 +16,16 @@ public class UrinalsTest {
         Urinals urinals = new Urinals();
         assertTrue(urinals.goodString("10001"));
         assertTrue(urinals.goodString("1001"));
-        assertTrue(urinals.goodString("1001"));
+        assertTrue(urinals.goodString("00000"));
         assertTrue(urinals.goodString("0000"));
         assertTrue(urinals.goodString("01000"));
-        assertTrue(urinals.goodString("011"));
     }
 
     @Test
     void badString() {
         Urinals urinals = new Urinals();
+        assertFalse(urinals.goodString("011"));
+        assertFalse(urinals.goodString("10001100010"));
         assertFalse(urinals.goodString("100201"));
         assertFalse(urinals.goodString("00000000000000000000000000000000"));
         assertFalse(urinals.goodString(""));
@@ -34,11 +35,31 @@ public class UrinalsTest {
         assertFalse(urinals.goodString("12417sd"));
     }
 
+    @Test
+    void countUrinals() {
+        Urinals urinals = new Urinals();
+        assertEquals(1, urinals.countUrinals("10001"));
+        assertEquals(0, urinals.countUrinals("1001"));
+        assertEquals(3, urinals.countUrinals("00000"));
+        assertEquals(2, urinals.countUrinals("0000"));
+        assertEquals(1, urinals.countUrinals("01000"));
+        assertEquals(-1,urinals.countUrinals("011"));
+        assertEquals(-1, urinals.countUrinals("100201"));
+        assertEquals(-1, urinals.countUrinals("00000000000000000000000000000000"));
+        assertEquals(-1, urinals.countUrinals(""));
+        assertEquals(-1, urinals.countUrinals("11111111111111111111111111111111"));
+        assertEquals(-1, urinals.countUrinals("10001100010"));
+        assertEquals(-1, urinals.countUrinals("     "));
+
+
+    }
+
     @Test()
-    void main(){
+     void main() {
         checkIfFileExists();
         goodString();
         badString();
+        countUrinals();
 
     }
 }
